0e75bfc7050fddffb5de01e70b8e27c25293f31c
Davit <gvelesiani99@gmail.com>
2022-10-13 15:08:49 +0200

    complleted

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
-function Signup() {
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate('/')
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className='w-full h-screen'>
-      <img
+        <img
           className='hidden sm:block absolute w-full h-full object-cover'
           src='https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg'
           alt='/'
@@ -15,10 +31,27 @@ function Signup() {
           <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
             <div className='max-w-[320px] mx-auto py-16'>
               <h1 className='text-3xl font-bold'>Sign Up</h1>
-              <form className='w-full flex flex-col py-4'>
-                <input className='p-3 my-2 bg-gray-700 rounded' type='email' placeholder='Email' autoComplete='email' />
-                <input className='p-3 my-2 bg-gray-700 rounded' type='password' placeholder='Password' autoComplete='current-password' />
-                <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign up</button>
+              <form
+                onSubmit={handleSubmit}
+                className='w-full flex flex-col py-4'
+              >
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  className='p-3 my-2 bg-gray-700 rouded'
+                  type='email'
+                  placeholder='Email'
+                  autoComplete='email'
+                />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  className='p-3 my-2 bg-gray-700 rouded'
+                  type='password'
+                  placeholder='Password'
+                  autoComplete='current-password'
+                />
+                <button className='bg-red-600 py-3 my-6 rounded font-bold'>
+                  Sign Up
+                </button>
                 <div className='flex justify-between items-center text-sm text-gray-600'>
                   <p>
                     <input className='mr-2' type='checkbox' />
@@ -38,7 +71,7 @@ function Signup() {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
